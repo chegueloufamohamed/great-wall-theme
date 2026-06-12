b14e3e0462d93e62542d038a8c359d9af4ce77e0
Nael advertising <naeladvertising@Mac.lan>
2026-06-12 12:57:29 +0400

    Create empty sidebar templates to override default WordPress widgets fallback

---
+++ sidebar-shop.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * WooCommerce Sidebar override to hide default widgets
+ *
+ * @package great-wall-theme
+ */
+
+// Silence is golden. We do not use sidebars in our modern design.
---
+++ sidebar.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Sidebar override to hide default WordPress widgets
+ *
+ * @package great-wall-theme
+ */
+
+// Silence is golden. We do not use sidebars in our modern design.
