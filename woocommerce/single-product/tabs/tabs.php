149588cafafcdd1962d5cb49705a08b2f5988e81
Nael advertising <naeladvertising@Mac.lan>
2026-07-21 12:59:07 +0400

    style: Justify tab panels text to left and force equal-width tabs in segmented control

--- woocommerce/single-product/tabs/tabs.php
+++ woocommerce/single-product/tabs/tabs.php
@@ -33,7 +33,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 	if ( ! empty( $product_tabs ) ) : ?>
 
-		<div class="woocommerce-tabs wc-tabs-wrapper" style="text-align: center !important;">
+		<div class="woocommerce-tabs wc-tabs-wrapper">
 			<ul class="tabs wc-tabs" role="tablist">
 				<?php foreach ( $product_tabs as $key => $product_tab ) : ?>
 					<li class="<?php echo esc_attr( $key ); ?>_tab" id="tab-title-<?php echo esc_attr( $key ); ?>" role="tab" aria-controls="tab-<?php echo esc_attr( $key ); ?>">
